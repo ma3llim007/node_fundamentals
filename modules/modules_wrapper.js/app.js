bdb0a924175ac22c30a3dea1efd10752a59adc1b
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-12 10:02:20 +0530

    learn the module wrapper function

---
+++ modules/modules_wrapper.js/app.js
@@ -0,0 +1,4 @@
+let a = 5;
+
+console.log("DIR Name ", __dirname);
+console.log("File Name ", __filename);
---
+++ modules/modules_wrapper.js/moduleWrapper.js
@@ -0,0 +1,1 @@
+// (function (exports, required, module, __filename, __dirname) {})(exports, required, module, __filename, __dirname);
