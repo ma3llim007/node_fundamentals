d23240fdece70f2a19a085b39cac7bfabad765b5
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-18 09:10:14 +0530

    learn ho w to access fileName and dirName in the es6 modules

---
+++ fileName_dirName_In_es6/app.js
@@ -0,0 +1,4 @@
+import { num } from "./math.js";
+
+const meta = import.meta;
+console.log(meta);
---
+++ fileName_dirName_In_es6/math.js
@@ -0,0 +1,5 @@
+export const num = 677;
+
+const meta = import.meta;
+console.log(meta.filename);
+console.log(meta.dirname);
