8747d03345243d8f901fc7d2a587a87d053120c1
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-04 10:29:21 +0530

    chore: learn the fs promises

--- fs_module/app.js
+++ fs_module/app.js
@@ -1,4 +1,12 @@
-import fs from "node:fs";
+// import fs from "node:fs";
+import fs from "node:fs/promises";
 
-const content = fs.readFileSync("fs_module\\text.txt", "utf8");
-console.log(content);
+// const content = fs.readFileSync("fs_module\\text.txt", "utf8");
+// fs.readFile("./fs_module/text.txt", (err, data) => {
+//     const content = data.toString();
+//     console.log(content);
+// });
+
+const a = await fs.readFile("./fs_module/text.txt");
+console.log(a.toString());
+console.log("End");
